380db26de0f4d4010726379bb0e8db45727fd03f
josno <jnsenoren@gmail.com>
2020-07-16 22:34:31 +0400

    Created component for Pokemon information

--- src/App.js
+++ src/App.js
@@ -1,41 +1,14 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import "./App.css";
+
+import PokemonInfo from "./components/PokemonInfo";
 
 function App() {
 	const [pokemonId, setPokemonId] = useState(1);
-	const [currentPokemon, setCurrentPokemon] = useState({
-		name: "",
-		weight: "",
-		height: "",
-		imgUrl: "",
-	});
-
-	useEffect(() => {
-		fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
-			.then((response) => response.json())
-			.then((responseJson) =>
-				setCurrentPokemon({
-					name: responseJson.name,
-					weight: responseJson.weight,
-					height: responseJson.height,
-					imgUrl: responseJson.sprites.front_default,
-				})
-			);
-	}, [pokemonId]);
 
 	return (
 		<div className='App'>
-			<div>
-				<img
-					src={currentPokemon.imgUrl}
-					alt={`${currentPokemon.name} Default`}
-				/>
-				<h1>{currentPokemon.name}</h1>
-				<ul>
-					<li>Weight: {currentPokemon.weight}</li>
-					<li>Height: {currentPokemon.height}</li>
-				</ul>
-			</div>
+			<PokemonInfo pokemonId={pokemonId} />
 		</div>
 	);
 }

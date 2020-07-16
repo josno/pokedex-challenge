d4a5d20ebf66f73e86a7f6621c2933f2b86e4bdb
josno <jnsenoren@gmail.com>
2020-07-16 22:23:53 +0400

    Checking out API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,43 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
 
 function App() {
-	return <div className='App'>Hello</div>;
+	const [pokemonId, setPokemonId] = useState(1);
+	const [currentPokemon, setCurrentPokemon] = useState({
+		name: "",
+		weight: "",
+		height: "",
+		imgUrl: "",
+	});
+
+	useEffect(() => {
+		fetch(`https://pokeapi.co/api/v2/pokemon/${pokemonId}`)
+			.then((response) => response.json())
+			.then((responseJson) =>
+				setCurrentPokemon({
+					name: responseJson.name,
+					weight: responseJson.weight,
+					height: responseJson.height,
+					imgUrl: responseJson.sprites.front_default,
+				})
+			);
+	}, [pokemonId]);
+
+	return (
+		<div className='App'>
+			<div>
+				<img
+					src={currentPokemon.imgUrl}
+					alt={`${currentPokemon.name} Default`}
+				/>
+				<h1>{currentPokemon.name}</h1>
+				<ul>
+					<li>Weight: {currentPokemon.weight}</li>
+					<li>Height: {currentPokemon.height}</li>
+				</ul>
+			</div>
+		</div>
+	);
 }
 
 export default App;

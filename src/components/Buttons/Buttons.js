1e4a9875942a7e35e939b9f468f66f126ffbe712
josno <jnsenoren@gmail.com>
2020-07-18 00:57:42 +0400

    Added icons as buttons

--- src/components/Buttons/Buttons.js
+++ src/components/Buttons/Buttons.js
@@ -1,10 +1,23 @@
 import React from "react";
+import { AiOutlineCaretLeft, AiOutlineCaretRight } from "react-icons/ai";
+
+import "./Buttons.css";
 
 const Buttons = (props) => {
 	return (
 		<div className='button-container'>
-			<button onClick={() => props.goToPreviousPokemon()}>{"<<"}</button>
-			<button onClick={() => props.goToNextPokemon()}>{">>"}</button>
+			<button
+				className='arrow-button-box'
+				onClick={() => props.goToPreviousPokemon()}
+			>
+				<AiOutlineCaretLeft className='arrow-style' />
+			</button>
+			<button
+				className='arrow-button-box'
+				onClick={() => props.goToNextPokemon()}
+			>
+				<AiOutlineCaretRight className='arrow-style' />
+			</button>
 		</div>
 	);
 };

3abf24d294db297954c4f9a2c9165ecad3358cfc
josno <jnsenoren@gmail.com>
2020-07-20 20:21:39 +0400

    Added buttons component to search bar

--- src/components/Searchbar/Searchbar.js
+++ src/components/Searchbar/Searchbar.js
@@ -1,17 +1,31 @@
 import React, { useState } from "react";
+import Buttons from "../Buttons/Buttons";
 
 const Searchbar = (props) => {
 	const [pokemon, setPokemon] = useState("");
+	const [searchbarToggle, setSearchbarToggle] = useState(false);
+
 	return (
 		<div className='search-bar-wrapper'>
-			<div className='search-bar-container'>
-				<input
-					className='search-input'
-					value={pokemon}
-					onChange={(e) => setPokemon(e.target.value)}
-				/>
-				<button onClick={() => props.onSearch(pokemon)}>Search</button>
-			</div>
+			{searchbarToggle ? (
+				<div className='search-bar-container'>
+					<input
+						className='search-input'
+						value={pokemon}
+						onChange={(e) => setPokemon(e.target.value)}
+					/>
+					<Buttons
+						goBack={() => setSearchbarToggle(!searchbarToggle)}
+						goTo={() => props.onSearch(pokemon)}
+						goToIcon={"Search"}
+						goBackIcon={"Cancel"}
+					/>
+				</div>
+			) : (
+				<button onClick={() => setSearchbarToggle(!searchbarToggle)}>
+					Search Database
+				</button>
+			)}
 		</div>
 	);
 };

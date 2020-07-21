23c9c9637d85bd217e056c37106aec3cef74de3f
josno <jnsenoren@gmail.com>
2020-07-21 23:12:55 +0400

    Styling in progress
    Added semantic html

--- src/components/Pokedex/Pokedex.js
+++ src/components/Pokedex/Pokedex.js
@@ -3,8 +3,10 @@ import "./Pokedex.css";
 
 const Pokedex = (props) => {
 	return (
-		<div className='pokedex-wrapper'>
-			<div className='pokedex-container'>
+		// <div className='pokedex-wrapper'>
+		<div className='pokedex-container'>
+			<header>
+				<h1 className='pokedex-title'>Pokedex 151</h1>
 				<div className='top-design'>
 					<div className='big-blue-light' />
 					<div className='pokedex-three-lights'>
@@ -13,14 +15,15 @@ const Pokedex = (props) => {
 						<div className='green-light light-circles' />
 					</div>
 				</div>
-				<div className='border-shadow-lines'>
-					<div className='border-line-one' />
-					<div className='border-line-two' />
-					<div className='border-line-three' />
-				</div>
-				<div className='pokemon-details-wrapper'>{props.children}</div>
-			</div>
+			</header>
+			{/* <div className='border-shadow-lines'>
+				<div className='border-line-one' />
+				<div className='border-line-two' />
+				<div className='border-line-three' />
+			</div> */}
+			<div className='pokemon-details-wrapper'>{props.children}</div>
 		</div>
+		// </div>
 	);
 };
 

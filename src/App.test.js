85f39c9220778f927c2f07e4919b139d9f9c11bc
josno <jnsenoren@gmail.com>
2020-07-24 00:55:26 +0400

    Added a render test

--- src/App.test.js
+++ src/App.test.js
@@ -1,24 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Pokedex from "./components/Pokedex/Pokedex";
+import PokemonInfo from "./components/PokemonInfo/PokemonInfo";
+import Searchbar from "./components/Searchbar/Searchbar";
+import Message from "./components/Message/Message";
 
-const responsejson = [
-	{ name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
-	{ name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
-	{ name: "venusaur", url: "https://pokeapi.co/api/v2/pokemon/3/" },
-	{ name: "charmander", url: "https://pokeapi.co/api/v2/pokemon/4/" },
-	{ name: "charmeleon", url: "https://pokeapi.co/api/v2/pokemon/5/" },
-	{ name: "charizard", url: "https://pokeapi.co/api/v2/pokemon/6/" },
-	{ name: "squirtle", url: "https://pokeapi.co/api/v2/pokemon/7/" },
-	{ name: "wartortle", url: "https://pokeapi.co/api/v2/pokemon/8/" },
-	{ name: "blastoise", url: "https://pokeapi.co/api/v2/pokemon/9/" },
-	{ name: "caterpie", url: "https://pokeapi.co/api/v2/pokemon/10/" },
-];
+import { mount, shallow } from "enzyme";
+import toJson from "enzyme-to-json";
 
 describe("App", () => {
 	it(`renders without crashing`, () => {
 		const div = document.createElement("div");
 		ReactDOM.render(<App />, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it(`renders child components correctly`, () => {
+		const wrapper = mount(<App />);
+		expect(wrapper.find("Pokedex").exists()).toBeTruthy();
+		expect(wrapper.find("Searchbar").exists()).toBeTruthy();
+		expect(wrapper.find("Buttons").exists()).toBeTruthy();
+		expect(wrapper.find("PokemonInfo").exists()).toBeTruthy();
 	});
 });

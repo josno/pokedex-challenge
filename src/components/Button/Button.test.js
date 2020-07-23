d9048df983a17b232e1ce84fa1aca03c83730f19
josno <jnsenoren@gmail.com>
2020-07-23 21:27:46 +0400

    Added tests

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Button from "./Button";
 
+import { mount } from "enzyme";
+
 describe("Button", () => {
 	it(`renders without crashing`, () => {
 		const div = document.createElement("div");
 		ReactDOM.render(<Button />, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it(`renders button label props`, () => {
+		const labelProps = "Click";
+		const container = mount(<Button label={labelProps} />);
+		expect(container.find("button").props().children).toBe(labelProps);
 	});
 });
--- src/components/Message/Message.test.js
+++ src/components/Message/Message.test.js
@@ -2,10 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 import Message from "./Message";
 
+import { shallow } from "enzyme";
+
 describe("Message", () => {
 	it(`renders without crashing`, () => {
 		const div = document.createElement("div");
 		ReactDOM.render(<Message />, div);
 		ReactDOM.unmountComponentAtNode(div);
+	});
+
+	it(`Given props renders passed message`, () => {
+		const messageProps = "Something wrong happened.";
+		const wrapper = shallow(<Message errorMessage={messageProps} />);
+		expect(wrapper.find("p").props().children).toBe(messageProps);
 	});
 });
